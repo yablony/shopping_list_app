ce43d2e53cfd73aebd0fd402ab16f340e15a5cdb
yablony <yablonskaya.yuliya@gmail.com>
2023-05-16 10:10:36 +0930

    Removed unnecesary lines from the schema file.

--- db/schema.sql
+++ db/schema.sql
@@ -11,8 +11,7 @@ CREATE TABLE users(
 CREATE TABLE shopping_list (
     id SERIAL PRIMARY KEY,
     user_id INTEGER,
-    item_name TEXT,
-    count INTEGER
+    item_name TEXT
 );
 
 CREATE TABLE fridge_items (
@@ -20,7 +19,3 @@ CREATE TABLE fridge_items (
     user_id INTEGER,
     item_name TEXT
 );
-
-INSERT INTO shopping_list(user_id, item_name, image_url)
-VALUES
-  (1, 'parsley', 'https://images.unsplash.com/photo-1588879460618-9249e7d947d1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80');
