f4e0787d013c1e3f17ec927a3138008bc7c1e82c
yablony <yablonskaya.yuliya@gmail.com>
2023-05-16 09:32:07 +0930

    Fixed the error with the route when updating an item on the fridge page.

--- controllers/fridge_controllers.py
+++ controllers/fridge_controllers.py
@@ -14,12 +14,22 @@ def new():
         fridge_list.append(fridge_item['item_name'])
     if item_name != '' and item_name not in fridge_list:
         create(session['user_id'], item_name)
+    return redirect('/')
+
+def new_inside_the_edit_page():
+    item_name = request.form.get('item_name')
+    fridge = select_all_fridge(session['user_id'])
+    fridge_list = []
+    for fridge_item in fridge:
+        fridge_list.append(fridge_item['item_name'])
+    if item_name != '' and item_name not in fridge_list:
+        create(session['user_id'], item_name)
     return redirect('/fridge/update')
 
 def edit(id):
     item_name = request.form.get('item_name')
     update_info(id, session['user_id'], item_name)
-    return render_template('/')
+    return redirect('/fridge/update')
 
 def delete_item(id):
     delete_fridge_item(id)
--- routes/fridge_routes.py
+++ routes/fridge_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from controllers.fridge_controllers import update, new, edit, delete_item, delete_all
+from controllers.fridge_controllers import update, new, edit, delete_item, delete_all, new_inside_the_edit_page
 
 fridge_routes = Blueprint('fridge_routes', __name__)
 
@@ -8,3 +8,4 @@ fridge_routes.route('/update')(update)
 fridge_routes.route('/<id>', methods=["POST"])(edit)
 fridge_routes.route('/<id>/delete', methods=['POST'])(delete_item)
 fridge_routes.route('/delete', methods=['POST'])(delete_all)
+fridge_routes.route('/new_inside_edit', methods=['POST'])(new_inside_the_edit_page)
